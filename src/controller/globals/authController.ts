f1d0c8ef0d1ecc8bc656286a3277bec995331a43
Santosh Joshi <regmisantox1411@gmail.com>
2025-06-16 21:48:53 +0545

    login created

--- src/controller/globals/authController.ts
+++ src/controller/globals/authController.ts
@@ -1,7 +1,7 @@
 
 /* 
 REGISTER/SIGNUP
--> incoming data -->username,email,password
+-> incoming data -->username,email,password->accept
 ->processing/checking --> eamil valid, compulsory data aaunai paryo
 -> db ->table ->query --> table maa insert/read/delete/update
 
@@ -67,6 +67,41 @@ static async registerUser(req:Request,res:Response){
       message : "User registered successfully."
     }) 
   }
+
+  async loginUser(req:Request,res:Response){
+    const {email,password} = req.body
+    if (!email||!password) {
+      res.status(400).json({
+        message : "Please provide username, password"
+      })
+      return
+    }
+    //now check if email exist or not in our users table
+    const data = await User.findAll({
+      where :{
+        email : email
+      }
+    })
+    if (data.length===null) {
+      res.status(404).json({
+        message : "Not registered!"
+      })
+      
+    }else{ //check password
+      //compare(plain pasword user bata aako,hashed password register huda users table maa baseko)
+      const isPassMatch = bcrypt.compareSync(password,data[0].password)
+      if (isPassMatch) { //login vayo, token generation
+        
+      } else{
+        res.status(403).json({
+          message : "Invalid email or password"
+        })
+      }
+
+    }
+
+  }
+
 }  //class based programming -> yesore garda nee vayo ->better to use this approach
 
 //export{registerUser}
